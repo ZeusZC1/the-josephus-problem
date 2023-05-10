00a682dd5b5d92ae6b241c6a13fdf036bbbc189a
ZeusZC1 <77069389+ZeusZC1@users.noreply.github.com>
2023-05-10 10:42:03 +0300

    Update the-josephus-problem.cpp

--- the-josephus-problem.cpp
+++ the-josephus-problem.cpp
@@ -1,32 +1,42 @@
 #include <stdio.h>
 
-int total = 41;
+#define TOTAL_PEOPLE 41
+#define STEP 2
 
+int main() {
+    int circle[TOTAL_PEOPLE];
+    int remaining_people = TOTAL_PEOPLE;
+    int index = 0;
 
-
-int main(){
-int matrix[total];
-for(int i = 0;i < total;i++){
-    matrix[i] = 1;
-}
-
-for(int i = 0; i < 9999; i++){
-   int j = 0;
-   if( matrix[i % (total)] == 1)
-   while(1){
-      if(j > total)
-         break;
-      if(matrix[(i + j + 1) % (total)] == 1 && (i + j + 1) % (total) != i % (total)){
-         matrix[(i + j + 1) %        (total)] = 0;
-         break;
-         }
-      j++;
+    // Initialize the circle with all people "alive" (1)
+    for (int i = 0; i < TOTAL_PEOPLE; i++) {
+        circle[i] = 1;
     }
-}
 
-for(int i = 0;i < total;i++){
-    if(matrix[i] == 1)
-    printf("Index %d is alive",i + 1);
-}
+    // Continue until only one person remains
+    while (remaining_people > 1) {
+        int steps_taken = 0;
 
+        // Move to the next alive person
+        while (steps_taken < STEP) {
+            index = (index + 1) % TOTAL_PEOPLE;
+            if (circle[index] == 1) {
+                steps_taken++;
+            }
+        }
+
+        // Eliminate the person
+        circle[index] = 0;
+        remaining_people--;
+    }
+
+    // Find the last remaining person and print the result
+    for (int i = 0; i < TOTAL_PEOPLE; i++) {
+        if (circle[i] == 1) {
+            printf("Index %d is alive\n", i + 1);
+            break;
+        }
+    }
+
+    return 0;
 }
